dad00af6de80b0ad592bafccfa524e76790c53ad
eprochnau <proc0035@algonquinlive.com>
2014-09-30 11:00:46 -0400

    changes made to css and js, removed typos

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var salesLeft = $('.sales-left');
 
 var detailsLeft = $('.details-left');
 
-var detailsLeft = function () {
+var detailsLeftClickHandler = function () {
 	var isActive = detailsLeft.attr('data-state');
 	
 	if (isActive == 'active') {
@@ -22,9 +22,9 @@ salesLeft.on('click', detailsLeftClickHandler);
 /*SALES AND DETAILS RIGHT*/
 var salesRight = $('.sales-right');
 
-var detailsRight = $('details-right');
+var detailsRight = $('.details-right');
 
-var detailsRight = function () {
+var detailsRightClickHandler = function () {
 	var isActive = detailsRight.attr('data-state');
 	
 	if (isActive == 'active') {
