41739bb23a8b48d47d3f43fe5982852db89c678b
eprochnau <proc0035@algonquinlive.com>
2014-09-30 10:41:37 -0400

    edited html, css, js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+/*SALES AND DETAILS LEFT*/
+var salesLeft = $('.sales-left');
+
+var detailsLeft = $('.details-left');
+
+var detailsLeft = function () {
+	var isActive = detailsLeft.attr('data-state');
+	
+	if (isActive == 'active') {
+		detailsLeft.attr('data-state', 'inactive');
+	} else {
+		detailsLeft.attr('data-state', 'active');
+	}
+};
+
+salesLeft.on('click', detailsLeftClickHandler);
+
+
+
+/*SALES AND DETAILS RIGHT*/
+var salesRight = $('.sales-right');
+
+var detailsRight = $('details-right');
+
+var detailsRight = function () {
+	var isActive = detailsRight.attr('data-state');
+	
+	if (isActive == 'active') {
+		detailsRight.attr('data-state', 'inactive');
+	} else {
+		detailsRight.attr('data-state', 'active');
+	}
+};
+
+salesRight.on('click', detailsRightClickHandler);
